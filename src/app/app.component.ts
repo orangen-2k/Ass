5a81758d35cadc3310a331d6e9562eca56e67f18
orangen-2k <orangen2000@gmail.com>
2020-03-25 21:12:15 +0700

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  title = "Thông tin danh sách bệnh viện hiện nay";
 }
