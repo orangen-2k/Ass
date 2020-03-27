f5eff51d87972244dcf40e2b85ac8ffc8737b9c2
orangen-2k <orangen2000@gmail.com>
2020-03-27 11:51:54 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component,OnInit } from '@angular/core';
 import {PtService} from './pt.service';
-import { Lophoc } from './lophoc';
 
 @Component({
   selector: 'my-app',
@@ -10,58 +9,8 @@ import { Lophoc } from './lophoc';
 export class AppComponent  {
   title = "Danh sách trường đại học thành phố Hà Nội";
 
-  datalh: Lophoc[];
-  constructor(
-    private ptService: PtService
-  ){}
+  constructor(){}
 
-  getdatalh(){
-   this.datalh = this.ptService.getdatalh();
-   console.log(this.ptService)
-  }
   ngOnInit() {
-    this.getdatalh();
   }
-  chuy(chuyen){
-    this.data = chuyen;
-  }
-  
-  cancel(){
-    this.data = {
-    id: null,
-    name: null,
-    room_number: null,
-    total_student: null,
-    schoolId: null,
-    main_teacher: null,
-    }
-  }
-  
-  data = {
-    id: null,
-    name: null,
-    room_number: null,
-    total_student: null,
-    schoolId: null,
-    main_teacher: null,
-  }
-  themtt(){
-    if(this.data.id == null){
-      let maxId = 0;
-      this.datalh.forEach(function(item){
-        if(item.id >= maxId){
-          maxId = item.id;
-        }
-      });
-      this.data.id = ++maxId;
-      this.datalh.push(this.data);
-    }
-    this.cancel();
-  }
-
-  xoatt(chuyen){
-    this.datalh = this.datalh.filter(function(item){
-      return item != chuyen;
-    });
-  } 
 }
