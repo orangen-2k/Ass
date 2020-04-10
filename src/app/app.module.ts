9455e8553a7d8b7070b7967699f0a4a89357a473
orangen-2k <orangen2000@gmail.com>
2020-04-10 17:49:55 +0700

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,19 @@ import { PtService } from './pt.service';
 import { SchoolsComponent } from './schools/schools.component';
 import { SchoolsCtComponent } from './schools-ct/schools-ct.component';
 import { ClassCtComponent } from './class-ct/class-ct.component';
+import { ShowallComponent } from './showall/showall.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule, FormsModule , HttpClientModule,ReactiveFormsModule,
     RouterModule.forRoot([
       {path: '', component: SchoolsComponent},
+      {path: 'show', component: ShowallComponent},
       {path: 'school/:schoolId', component: SchoolsCtComponent},
       {path: "school/:schoolId/class/:classId",component: ClassCtComponent},
     ])
     ],
-  declarations: [ AppComponent, SchoolsComponent, SchoolsCtComponent, ClassCtComponent ],
+  declarations: [ AppComponent, SchoolsComponent, SchoolsCtComponent, ClassCtComponent, ShowallComponent ],
   bootstrap:    [ AppComponent ],
   providers: [PtService]
 })
