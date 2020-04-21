0913d483d1d5b442b6c367848e2b8319082a1228
orangen-2k <orangen2000@gmail.com>
2020-04-21 10:02:12 +0700

    4 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { AppComponent } from './app.component';
 import { PtService } from './pt.service';
 import { SchoolsComponent } from './schools/schools.component';
 import { SchoolsCtComponent } from './schools-ct/schools-ct.component';
-import { ClassCtComponent } from './class-ct/class-ct.component';
 import { ShowallComponent } from './showall/showall.component';
 import { AddComponent } from './add/add.component';
 
@@ -21,10 +20,9 @@ import { AddComponent } from './add/add.component';
       {path: 'show', component: ShowallComponent},
       {path: 'add', component: AddComponent},
       {path: 'school/:schoolId', component: SchoolsCtComponent},
-      {path: "school/:schoolId/class/:classId",component: ClassCtComponent},
     ])
     ],
-  declarations: [ AppComponent, SchoolsComponent, SchoolsCtComponent, ClassCtComponent, ShowallComponent, AddComponent ],
+  declarations: [ AppComponent, SchoolsComponent, SchoolsCtComponent, ShowallComponent, AddComponent ],
   bootstrap:    [ AppComponent ],
   providers: [PtService]
 })
